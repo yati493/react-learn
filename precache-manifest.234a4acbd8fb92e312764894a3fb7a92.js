4466e1451493d86ba7343ed5536cf2866c0801a9
Yash Garg <yash.garg@airtel.com>
2019-07-08 12:55:48 +0530

    Updates

---
+++ precache-manifest.234a4acbd8fb92e312764894a3fb7a92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5976d8884d418d8483dec5fd0722e495",
+    "url": "/react-learn/index.html"
+  },
+  {
+    "revision": "16b44506f5dda0268378",
+    "url": "/react-learn/static/css/main.596c529e.chunk.css"
+  },
+  {
+    "revision": "b8c06ae2654d6d74b609",
+    "url": "/react-learn/static/js/2.833e2436.chunk.js"
+  },
+  {
+    "revision": "16b44506f5dda0268378",
+    "url": "/react-learn/static/js/main.a5835189.chunk.js"
+  },
+  {
+    "revision": "26b3fe974fe2977cb2dc",
+    "url": "/react-learn/static/js/runtime~main.2282ddb6.js"
+  }
+]);
